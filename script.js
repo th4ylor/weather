b86931dbbe491fd64f8463e75a3cf7ea3ede9dae
isaak <isaak@casanova.imb.br>
2026-07-11 12:31:19 -0300

    adicionando relogio

--- script.js
+++ script.js
@@ -13,7 +13,34 @@ async function buttonClick() {
     let respostaServidor = await fetch(endereço)
     let dadosJson = await respostaServidor.json()
     console.log(dadosJson)
+    const clock = document.getElementById("clock")
+    
+    const hr = document.getElementById("hr")
+    const min = document.getElementById("min")
+    const sec = document.getElementById("sec")
 
+    clock.style.display = "flex"
+
+    rotation = (target, val) => {
+        target.style.transform = `rotate(${val}deg)`
+    }
+    clock = () => {
+        let date = new Date()
+        let hh = (date.getHours() % 12) + date.getMinutes() / 59
+        let mm = date.getMinutes()
+        let ss = date.getSeconds()
+
+        hh *= 30
+        mm *= 6
+        ss *= 6
+
+        rotation(hr, hh)
+        rotation(min, mm)
+        rotation(sec, ss)
+
+        setTimeout(clock,500)
+    }
+    window.onload = clock()
     
 
     caixa.innerHTML = `
